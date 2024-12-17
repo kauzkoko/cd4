c83fd95b94c487bf1b74647c83a3bab5021df6e6
kauzkoko <kamelkebab@tutanota.com>
2024-12-17 11:33:36 +0900

    feat: cd2 stuff

---
+++ components/Scenes/CD2.vue
@@ -0,0 +1,36 @@
+<template>
+  <TresPerspectiveCamera ref="camera" :position="[0, 0, 3]" :lookAt="[0, 0, 0]" />
+  <TresMesh position:[0, 0, 0] :rotation="[0, 0.1, 0]">
+    <TresBoxGeometry />
+    <TresMeshNormalMaterial :opacity="0" transparent />
+    <Html center prepend transform :distance-factor="1" wrapperClass="test">
+      <html :style="{ width: `${width}px`, height: `${height}px` }" class="flexCenter bg-red">
+        <div class="bg-black text-amber md:bg-blue">stuff</div>
+      </html>
+    </Html>
+  </TresMesh>
+  <!-- <GridHelper /> -->
+  <MouseParallax :factor="0.1" :local="false" />
+  <LightsBasic />
+</template>
+
+<script setup>
+import { Html } from '@tresjs/cientos'
+
+const { x, y } = useMouse()
+const { width, height } = useWindowSize()
+const camera = ref(null)
+
+const opacity = ref(1)
+onMounted(() => {
+  watch([width, height], () => {
+    console.log('changed')
+    opacity.value = 0
+})
+</script>
+
+<style>
+.test {
+  transition: opacity 0.3s ease-out;
+}
+</style>
---
+++ pages/conv2/tres.vue
@@ -0,0 +1,19 @@
+<template>
+  <TresCanvas v-bind="gl">
+    <ScenesCD2 />
+  </TresCanvas>
+</template>
+
+<script setup>
+const gl = {
+  clearColor: "white",
+  shadows: true,
+  alpha: true,
+  windowSize: true,
+};
+
+const { width, height } = useWindowSize()
+watch([width, height], () => {
+
+})
+</script>
